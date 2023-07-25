90d10701521c158cf1438efbc91d6925d8e3809e
Luis Gaudencio <luis_g24@live.com>
2023-07-25 10:34:14 +0000

    Add while loop for main menu and add options

--- run.py
+++ run.py
@@ -6,3 +6,12 @@ def introduction():
 
 
 def main_menu():
+    response = ""
+    while response not in MAIN_MENU_OPTIONS:
+        print("MAIN MENU")
+        response = input("""Please select an option below:
+    
+Enter 1 - About This Game
+Enter 2 - Game Instructions
+Enter 3 - Play Game
+""")
