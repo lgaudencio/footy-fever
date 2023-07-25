72ffba20fd46b9edd40a7041b713b619e75de313
Luis Gaudencio <luis_g24@live.com>
2023-07-25 21:07:27 +0000

    Add text to display stats to user in game

--- stats.py
+++ stats.py
@@ -6,3 +6,10 @@ def club_one_stats():
     games_played = random.randint(140, 160)
     ratio_calc = (goals + assists) / games_played
     ratio = round(ratio_calc, 2)
+
+    print(f"""Your stats the last five seasons:
+  
+Games Played: {games_played} 
+Goals Scored: {goals} 
+Assists: {assists} 
+Total Goal Involvments per 90: {ratio}""")
