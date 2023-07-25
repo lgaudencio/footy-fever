bf31c2906441dbe7185a5db1611fa9186700f6f3
Luis Gaudencio <luis_g24@live.com>
2023-07-25 21:00:50 +0000

    Add calculation for goals per game ratio

--- stats.py
+++ stats.py
@@ -4,3 +4,5 @@ def club_one_stats():
     goals = random.randint(65, 85)
     assists = random.randint(30, 50)
     games_played = random.randint(140, 160)
+    ratio_calc = (goals + assists) / games_played
+    ratio = round(ratio_calc, 2)
