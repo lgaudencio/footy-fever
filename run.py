5a5812eb710a60703f83309565e6af020590fe84
Luis Gaudencio <luis_g24@live.com>
2023-07-25 10:39:46 +0000

    Add if...elif...else statements to the while loop for main menu

--- run.py
+++ run.py
@@ -15,3 +15,11 @@ Enter 1 - About This Game
 Enter 2 - Game Instructions
 Enter 3 - Play Game
 """)
+        if response == "1":
+            print("About This Game:")
+        elif response == "2":
+            print("Game instructions")
+        elif response == "3":
+            print("Starting Game...")
+        else:
+            print("Please enter a number between 1 and 3")
