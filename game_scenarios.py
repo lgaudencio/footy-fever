9fe6e0eb3901d3b9bcdc0bd1761aa3ad3d7d828d
Luis Gaudencio <luis_g24@live.com>
2023-07-26 21:32:33 +0000

    Add if elif else conditions to while loop for scenario

--- game_scenarios.py
+++ game_scenarios.py
@@ -7,3 +7,9 @@ def in_game_scenario_one():
     chance = random.randint(0, 1)
     while response not in YES_OR_NO:
         response = input("You've been fouled inside the box do you chose to take the penatlty?... Please enter yes or no...")
+        if response.upper() == "NO":
+        
+        elif response.upper() == "YES":
+
+        else:
+            print("Please type yes or no...")
