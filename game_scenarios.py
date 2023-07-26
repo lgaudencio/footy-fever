ce990acfe3a8ec7e8beccf2d69b5658bbd66a557
Luis Gaudencio <luis_g24@live.com>
2023-07-26 21:27:41 +0000

    Add while loop for game scenario and add chance variable for outcome

--- game_scenarios.py
+++ game_scenarios.py
@@ -3,3 +3,7 @@ import random
 YES_OR_NO = ["YES", "NO"]
 
 def in_game_scenario_one():
+    response = ""
+    chance = random.randint(0, 1)
+    while response not in YES_OR_NO:
+        response = input("You've been fouled inside the box do you chose to take the penatlty?... Please enter yes or no...")
