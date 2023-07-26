a46e6726236804e5d3350adb650fd27d1bcb5352
Luis Gaudencio <luis_g24@live.com>
2023-07-26 21:45:53 +0000

    Add nested if elif conditions for the 'no' response

--- game_scenarios.py
+++ game_scenarios.py
@@ -8,7 +8,13 @@ def in_game_scenario_one():
     while response not in YES_OR_NO:
         response = input("You've been fouled inside the box do you chose to take the penatlty?... Please enter yes or no...")
         if response.upper() == "NO":
-        
+            print("You handed the responsiviluty to you teammate...")
+            if chance == 0:
+                print("You teammate missed the penalty!")
+            elif chance == 1:
+                print("You teammate scored! Your team is now 1-0 up!")
+                break
+
         elif response.upper() == "YES":
 
         else:
