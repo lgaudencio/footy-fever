9902f339af1e7ee0afe2f2be79c3d6f6ce76ffe9
Luis Gaudencio <luis_g24@live.com>
2023-07-26 21:53:43 +0000

    Fix spacing on in game scenario one function

--- game_scenarios.py
+++ game_scenarios.py
@@ -14,7 +14,6 @@ def in_game_scenario_one():
             elif chance == 1:
                 print("You teammate scored! Your team is now 1-0 up!")
                 break
-
         elif response.upper() == "YES":
             if chance == 0:
                 print("You missed the penatly!")
@@ -22,6 +21,5 @@ def in_game_scenario_one():
             elif chance == 1:
                 print("You scored! Your team now leads 1-0")
                 break
-
         else:
             print("Please type yes or no...")
