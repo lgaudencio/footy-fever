375cdb3d96bab7b9fe3034abbdfce3efca038198
Luis Gaudencio <luis_g24@live.com>
2023-07-29 16:24:01 +0000

    Add at third club function

--- run.py
+++ run.py
@@ -265,6 +265,9 @@ If you accept the offer type 'YES' if you want to negotiate type 'NO'""")
     print(f"{name}, as you move to {third_club}, you are reminded with what you have achieved in the last five season with your previous club...")
 
     stat.club_two_stats()
+
+
+def at_third_club():
     
 
 def main():
