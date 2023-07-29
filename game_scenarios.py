7478bfca405eb89bc9e2de9dbe82fed78251898c
Luis Gaudencio <luis_g24@live.com>
2023-07-29 16:34:10 +0000

    Add while loop for third in game scenario

--- game_scenarios.py
+++ game_scenarios.py
@@ -54,3 +54,7 @@ def in_game_scenario_two():
 
 
 def in_game_scenario_three():
+    response = ""
+    chance = random.randint(0, 1)
+    while response not in YES_OR_NO:
+        response = input("An opposition player is in on goal, do you takle him?... Please enter yes or no...")
