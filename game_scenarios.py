e77d30912c4decb24a62dff80c0cfeb3199353d9
Luis Gaudencio <luis_g24@live.com>
2023-08-01 22:12:46 +0000

    Fix gameplay text on in game scenario two function

--- game_scenarios.py
+++ game_scenarios.py
@@ -45,7 +45,10 @@ def in_game_scenario_two():
     chance = random.randint(0, 1)
     distance = str(random.randint(20, 45)) + " Yards"
     while response not in SHOOT_OR_PASS:
-        response = input(f"Your team is battling it out to secure a Champions League spot for next season. A freekick has been awarded, it is {distance} out from goal. As the clubs freekick taker, you hover over the ball. Do you take a shot on goal or do you pass it?... Please enter shoot or pass...")
+        response = input(f"""Your team is battling it out to secure a Champions League spot for next season. 
+A freekick has been awarded, it is {distance} out from goal. 
+As the clubs freekick taker, you hover over the ball. Do you take a shot on goal or do you pass it?... 
+Please enter SHOOT or PASS...""")
         if response.upper() == "SHOOT":
             print("You shoot...")
             time.sleep(2)
