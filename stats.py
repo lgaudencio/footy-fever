d69047ea2df54f11cbf96d6bb990c0ecf97701af
Luis Gaudencio <luis_g24@live.com>
2023-08-01 20:40:57 +0000

    Add sleep function to club four stats function

--- stats.py
+++ stats.py
@@ -70,6 +70,8 @@ def club_four_stats():
     ratio_calc = (goals + assists) / games_played
     ratio = round(ratio_calc, 2)
 
+    time.sleep(3)
+
     print(f"""Your stats the last five seasons:
   
 Games Played: {games_played} 
