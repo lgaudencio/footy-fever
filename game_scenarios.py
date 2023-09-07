45baf72255248200fffd354562a9d048258b1c74
Luis Gaudencio <luis_g24@live.com>
2023-09-07 21:18:21 +0000

    Fix errors found by python linter on game scenarios file

--- game_scenarios.py
+++ game_scenarios.py
@@ -5,10 +5,11 @@ from colorama import Fore, Back, Style, init
 YES_OR_NO = ["YES", "NO"]
 SHOOT_OR_PASS = ["SHOOT", "PASS"]
 
+
 def in_game_scenario_one():
     """
     This function is used for the first in-game scenario, where
-    the user will have two options to chose from and output 
+    the user will have two options to chose from and output
     generated at random
     """
     time.sleep(3)
@@ -17,9 +18,11 @@ def in_game_scenario_one():
     response = ""
     chance = random.randint(0, 1)
     while response not in YES_OR_NO:
-        response = input("\nYou've been fouled inside the box do you chose to "
-                            "take the penalty or hand it to a teammate?...\n"
-                            "\nPlease enter YES to take the pentalty or NO to hand it over to your teammate...\n")
+        response = input(
+            "\nYou've been fouled inside the box do you chose to "
+            "take the penalty or hand it to a teammate?...\n"
+            "\nPlease enter YES to take the pentalty or NO to hand it over to "
+            "your teammate...\n")
         if response.upper() == "NO":
             print("\nYou handed the responsibility to your teammate...\n")
             time.sleep(2)
@@ -47,7 +50,7 @@ def in_game_scenario_one():
 def in_game_scenario_two():
     """
     This function is used for the second in-game scenario, where
-    the user will have two options to chose from and output 
+    the user will have two options to chose from and output
     generated at random
     """
     time.sleep(3)
@@ -57,11 +60,13 @@ def in_game_scenario_two():
     chance = random.randint(0, 1)
     distance = str(random.randint(20, 45)) + " Yards"
     while response not in SHOOT_OR_PASS:
-        response = input("\nYour team is battling to secure a Champions League spot for next season.\n"
-                            f"\nA freekick has been awarded, it is {distance} out from goal.\n"
-                            "\nAs the clubs freekick taker, you hover over the ball. "
-                            "Do you take a shot on goal or do you pass it?...\n"
-                            "\nPlease enter SHOOT or PASS...\n")
+        response = input(
+            "\nYour team is battling to secure a Champions League spot for "
+            "next season.\n"
+            f"\nA freekick has been awarded, it is {distance} out from goal.\n"
+            "\nAs the clubs freekick taker, you hover over the ball. "
+            "Do you take a shot on goal or do you pass it?...\n"
+            "\nPlease enter SHOOT or PASS...\n")
         if response.upper() == "SHOOT":
             print("\nYou shoot...\n")
             time.sleep(2)
@@ -70,16 +75,20 @@ def in_game_scenario_two():
                 break
             elif chance == 1:
                 print("...You score! Your team is now 3-2 up!\n")
-                break 
+                break
         elif response.upper() == "PASS":
             print("\nYou pass to one of your teammates...\n")
             time.sleep(2)
             if chance == 0:
-                print("An opposition player tackles your teammate, and gets the ball away!\n")
+                print(
+                    "An opposition player tackles your teammate, and gets "
+                    "the ball away!\n")
                 break
             elif chance == 1:
-                print("Your teammate headers the ball and it goes in! Your team is now 3-2 up!\n")
-                break        
+                print(
+                    "Your teammate headers the ball and it goes in! Your "
+                    "team is now 3-2 up!\n")
+                break
         else:
             print(Fore.RED + "\nPlease enter either SHOOT or PASS...\n")
 
@@ -89,7 +98,7 @@ def in_game_scenario_two():
 def in_game_scenario_three():
     """
     This function is used for the third in-game scenario, where
-    the user will have two options to chose from and output 
+    the user will have two options to chose from and output
     generated at random
     """
     time.sleep(3)
@@ -98,25 +107,34 @@ def in_game_scenario_three():
     response = ""
     chance = random.randint(0, 1)
     while response not in YES_OR_NO:
-        response = input("\nAn opposition player is in on goal, do you takle him?...\n"
-                            "\nPlease enter YES to tackle or NO to take no action...\n")
+        response = input(
+            "\nAn opposition player is in on goal, do you takle him?...\n"
+            "\nPlease enter YES to tackle or NO to take no action...\n")
         if response.upper() == "YES":
             print("You decided to make the tackle...\n")
             time.sleep(2)
             if chance == 0:
-                print("Bad tackle! You got none of the ball so the ref gave you a red card!\n")
+                print(
+                    "Bad tackle! You got none of the ball so the ref gave "
+                    "you a red card!\n")
                 break
             elif chance == 1:
                 print("You got the ball! Excellent tackle!\n")
                 break
         elif response.upper() == "NO":
-            print("\nThe opposition player has a great chance to score here...\n")
+            print(
+                "\nThe opposition player has a great chance to score "
+                "here...\n")
             time.sleep(2)
             if chance == 0:
-                print("The opposition player scored, your team is now 2-1 down!\n")
+                print(
+                    "The opposition player scored, your team is now 2-1 "
+                    "down!\n")
                 break
             elif chance == 1:
-                print("You goalkeeper made an incredible save, the score stays level at 1-1!\n")
+                print(
+                    "You goalkeeper made an incredible save, the score "
+                    "stays level at 1-1!\n")
                 break
         else:
             print(Fore.RED + "\nPlease enter either YES or NO...")
@@ -127,7 +145,7 @@ def in_game_scenario_three():
 def in_game_scenario_four():
     """
     This function is used for the fourth in-game scenario, where
-    the user will have two options to chose from and output 
+    the user will have two options to chose from and output
     generated at random
     """
     time.sleep(3)
@@ -136,10 +154,14 @@ def in_game_scenario_four():
     response = ""
     chance = random.randint(0, 1)
     while response not in SHOOT_OR_PASS:
-        response = input("\nYou are one-on-one with the goalkeeper, but the angle is getting tighter.\n"
-                            "\nYou see that if you pass to your teammate, they'll have an a clear cut "
-                            "opportunity to score. Do you take the shot or do you pass to teammate?...\n"
-                            "\nPlease enter SHOOT or PASS...\n")
+        response = input(
+            "\nYou are one-on-one with the goalkeeper, but the angle is "
+            "getting tighter.\n"
+            "\nYou see that if you pass to your teammate, they'll have an a "
+            "clear cut "
+            "opportunity to score. Do you take the shot or do you pass to "
+            "teammate?...\n"
+            "\nPlease enter SHOOT or PASS...\n")
         if response.upper() == "SHOOT":
             print("\nYou shoot...\n")
             time.sleep(2)
@@ -156,8 +178,10 @@ def in_game_scenario_four():
                 print("You teammate shoots the ball over the bar!\n")
                 break
             elif chance == 1:
-                print("Your teammate gets an easy tap in and your team now leads by 4-3!\n")
-                break          
+                print(
+                    "Your teammate gets an easy tap in and your team now "
+                    "leads by 4-3!\n")
+                break
         else:
             print(Fore.RED + "\nError: Please type SHOOT or PASS...")
 
